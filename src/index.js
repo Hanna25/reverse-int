a7681db4c17c16c2d0b40280050c4618bc5636e0
Anna <a.sobolenko1194@gmail.com>
2020-02-23 21:49:50 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    let positivNum = -n + ""
+    return +positivNum.split("").reverse().join("")  
+  }
+  let str = n + ""
+  return +str.split("").reverse().join("")
 }
